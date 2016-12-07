021c96081ddfb0ae661409742b872dcc4dbaadcd
zeitler2 <zeitler2@msu.edu>
2016-12-06 23:40:15 -0500

    Add several pages

---
+++ final_project/js/index.js
@@ -0,0 +1,17 @@
+
+	$('#oz-header').load('components/header.html');
+	$('#oz-menu').load('components/menu.html');
+	$('#oz-menu-bottom').load('components/menu.html');
+	$('#oz-featured-tiles').load('featured_tiles.html');
+	$('#oz-featured-reviews').load('featured_tiles.html');
+	$('#oz-featured-shares').load('featured_tiles.html');
+
+	function filter_articles(target, type) {
+		target.find('article').hide();
+		target.find(type).show();
+	}
+	
+$(document).ready( function() {
+	filter_articles($('#oz-featured-reviews'), '.tag-review');
+	filter_articles($('#oz-featured-shares'), '.tag-share');
+});
